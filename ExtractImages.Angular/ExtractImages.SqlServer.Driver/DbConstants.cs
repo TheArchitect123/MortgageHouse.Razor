419b47de13b8ef03df88c14c871f3f661dec4f7e
Dan Gerchcovich <dan.developer789@gmail.com>
2020-07-10 23:37:21 +1000

    Updated Connection String

--- ExtractImages.Angular/ExtractImages.SqlServer.Driver/DbConstants.cs
+++ ExtractImages.Angular/ExtractImages.SqlServer.Driver/DbConstants.cs
@@ -3,7 +3,7 @@
     public struct DbConstants
     {
         public const string ImageDirectory = "C:\\ExtractedImages\\";
-        public const string DbConnectionString = "Server=.\\SQLExpress;Database=MortgageHouse.Import;Trusted_Connection=True;Integrated Security=true;MultipleActiveResultSets=true";
+        public const string DbConnectionString = "Server=.\\SQLExpress;Database=mortgageHouseDb;Trusted_Connection=True;Integrated Security=true;MultipleActiveResultSets=true";
         public const string DbConnectionSchema = "dbo";
     }
 }
